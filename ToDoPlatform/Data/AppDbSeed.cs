85973d520064c637e8ff7439cf912a57e3ffd71f
AnthonyRodrigues894 <anthonyserrano894@gmail.com>
2026-03-10 10:47:48 -0300

    caralha do demonio das profundezas do inferno ao quadrado

--- ToDoPlatform/Data/AppDbSeed.cs
+++ ToDoPlatform/Data/AppDbSeed.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using ToDoPlatform.Models;
 
 namespace ToDoPlatform.Data;
 public class AppDbSeed
@@ -29,8 +30,96 @@ public class AppDbSeed
 
 
         #region Popular Usuário
+        List<AppUser> users = new()
+        {
+            new AppUser()
+            {
+                Id = "c46a77f6-3bcd-4a7b-b0ef-35bb55630fbb",
+                Email = "viniciusgodoy.martins@gmail.com",
+                NormalizedEmail = "VINICIUSGODOY.MARTINS@GMAIL.COM",
+                UserName = "viniciusgodoy.martins@gmail.com",
+                NormalizedUserName = "VINICIUSGODOY.MARTINS@GMAIL.COM",
+                LockoutEnabled = false,
+                EmailConfirmed = true,
+                Name = " Vinicius Godoy Martins",
+                ProfilePicture = "/img/users/caralha_do_demonio_das_profundezas_do_usuario_idiota"
+        }, 
 
+        new AppUser()
+            {
+                Id = "af249a93-6a3b-45f0-b097-440b8f8f0446",
+                Email = "anthonyserrano849@gmail.com",
+                NormalizedEmail = "ANTHONYSERRANO894@GMAIL.COM",
+                UserName = "anthonyserrano894@gmail.com",
+                NormalizedUserName = "ANTHONYSERRANO894@GMAIL.COM",
+                LockoutEnabled = true,
+                EmailConfirmed = true,
+                Name = " Anthony Rodrigues da Silva",
+                ProfilePicture = "/img/users/caralha_do_demonio_das_profundezas_do_usuario_idiota"
+        }, 
+        };
+
+
+        foreach(var user in users)
+        {
+            PasswordHasher<IdentityUser> pass = new();
+            user.PasswordHash = pass.HashPassword(user, "123456");
+        }
+        builder.Entity<AppUser>().HasData(users);
         #endregion
+
+
+
+        #region Popular Dados de Usuário Perfil
+        List<IdentityUserRole<string>> userRoles = new()
+        {
+            new IdentityUserRole<string>()
+            {
+                UserId = users[0].Id,
+                RoleId = users[0].Id,
+            },
+
+            new IdentityUserRole<string>()
+            {
+                UserId= users[1].Id,
+                RoleId = roles[1].Id
+            },
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
+        #endregion
+
+        #region  Popular as Tarefas do usuarío
+        List<ToDo> toDos = new()
+        {
+            new ToDo()
+            {
+                id=1,
+                Title=" Se matar",
+                Description= "cometer suicidio",
+                UserId= users[0].Id
+            },
+
+            new ToDo()
+            {
+                id=2,
+                Title=" Se matar",
+                Description= "cometer suicidio",
+                UserId= users[0].Id
+            },
+
+            new ToDo()
+            {
+                id=3,
+                Title=" Se matar",
+                Description= "cometer suicidio",
+                UserId= users[1].Id
+            },
+            
+        };
+        builder.Entity<ToDo>().HasData(toDos);
+        #endregion
+
+
     }    
 }
 
